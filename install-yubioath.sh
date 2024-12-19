0df2a4f9afebc4499afa8e0fff1acc68b4c0998e
H <haiken@haiken-thinkpad.openaiken.internal>
2024-12-19 13:21:51 -0600

    qol changes

--- install-yubioath.sh
+++ install-yubioath.sh
@@ -5,10 +5,12 @@ URL=https://developers.yubico.com/yubioath-flutter/Releases/yubico-authenticator
 FILENAME=yubico-auth-latest.tar.gz
 OUTDIR=yubi-auth-files
 
+pkill -f yubico
+
 curl -L $URL -o $DIR/$FILENAME
 mkdir $DIR/$OUTDIR
 tar --overwrite -xzf $DIR/$FILENAME -C $OUTDIR --strip-components 1
 sudo chown root:root $DIR/$OUTDIR/authenticator
-sudo chmod 555 $DIR/$OUTDIR/authenticator
+sudo chmod 755 $DIR/$OUTDIR/authenticator
 sudo ln -s $DIR/$OUTDIR/authenticator /usr/bin/yubioath-desktop
 bash $DIR/$OUTDIR/desktop_integration.sh -i
