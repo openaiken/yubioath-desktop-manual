0360d0db6b37a3e238df6ea4492fff18d7b5fc3a
H <haiken@haiken-thinkpad.openaiken.internal>
2024-12-19 13:23:08 -0600

    added -p

--- install-yubioath.sh
+++ install-yubioath.sh
@@ -8,7 +8,7 @@ OUTDIR=yubi-auth-files
 pkill -f yubico
 
 curl -L $URL -o $DIR/$FILENAME
-mkdir $DIR/$OUTDIR
+mkdir -p $DIR/$OUTDIR
 tar --overwrite -xzf $DIR/$FILENAME -C $OUTDIR --strip-components 1
 sudo chown root:root $DIR/$OUTDIR/authenticator
 sudo chmod 755 $DIR/$OUTDIR/authenticator
